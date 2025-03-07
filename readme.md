c64f0737e1e483a2f349018b760aeb1fc6bc0d31
mmheemskerk <michael.m.heemskerk@gmail.com>
2025-03-07 15:50:32 +0100

    readme and better contrast

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Michaels portfolio website
+
+This is a small vite website that uses lottie for a character animation and scss for styling.
+
+### to run
+```
+npm install
+npm run dev
+```
+
+### to build
+```
+npm run build
+```
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import './style.scss'
-import {loadAnimations, playMoonHover, playMoonToSun, playSunHover, playSunToMoon} from './animations'
+import "./style.scss";
+import {
+	loadAnimations,
+	playMoonHover,
+	playMoonToSun,
+	playSunHover,
+	playSunToMoon,
+} from "./animations";
 
 let themeIsDark = false;
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	switchTheme(themeIsDark = !themeIsDark);
+if (
+	window.matchMedia &&
+	window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+	switchTheme((themeIsDark = !themeIsDark));
 }
 
 // Load functions
@@ -13,42 +22,44 @@ listeners();
 
 function listeners() {
 	// Scroll down icon listener
-	let el = document.getElementById('scroll-down-arrow');
-	el.addEventListener('click', () => {
-		console.log(document.getElementById('scroll-target'));
-		document.getElementById('scroll-target').scrollIntoView({behavior: 'smooth'});
+	let el = document.getElementById("scroll-down-arrow");
+	el.addEventListener("click", () => {
+		console.log(document.getElementById("scroll-target"));
+		document
+			.getElementById("scroll-target")
+			.scrollIntoView({ behavior: "smooth" });
 	});
 
 	// Theme button listeners
-	let themeBtn = document.getElementById('theme-switcher');
-	themeBtn.addEventListener('click', () => {
-		themeIsDark = !themeIsDark
+	let themeBtn = document.getElementById("theme-switcher");
+	themeBtn.addEventListener("click", () => {
+		themeIsDark = !themeIsDark;
 		switchTheme(themeIsDark);
 	});
 
-	themeBtn.addEventListener('mouseenter', () => {
+	themeBtn.addEventListener("mouseenter", () => {
 		if (themeIsDark) {
 			playSunHover();
 		} else {
 			playMoonHover();
 		}
-	})
-};
+	});
+}
 
 function switchTheme(isDark) {
-	var r = document.querySelector(':root');
+	var r = document.querySelector(":root");
 
 	if (isDark) {
 		playMoonToSun();
-		r.style.setProperty('--background-color', '#1f1f1f');
-		r.style.setProperty('--text-color', '#f3f3f3');
-		r.style.setProperty('--accent-color', '#364CEA');
-		r.style.setProperty('--brightness', '1');
+		r.style.setProperty("--background-color", "#1f1f1f");
+		r.style.setProperty("--text-color", "#f3f3f3");
+		r.style.setProperty("--accent-color", "#F8E707");
+		r.style.setProperty("--brightness", "1");
 	} else {
 		playSunToMoon();
-		r.style.setProperty('--background-color', '#FDFDFD');
-		r.style.setProperty('--text-color', '#2C3E50');
-		r.style.setProperty('--accent-color', '#364CEA');
-		r.style.setProperty('--brightness', '.12');
+		r.style.setProperty("--background-color", "#FDFDFD");
+		r.style.setProperty("--text-color", "#2C3E50");
+		r.style.setProperty("--accent-color", "#364CEA");
+		r.style.setProperty("--brightness", ".12");
 	}
 }
